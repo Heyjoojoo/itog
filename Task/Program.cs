b54eca31954188e50b002666eedcf05b2c5b85bb
Marina <tiffuncile@yandex.ru>
2022-11-29 01:18:51 +0300

    Add Completed Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InPutArray(string [] oldArray, int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Пишите, не стесняйтесь: ");
+        string a = Console.ReadLine();
+        oldArray[i] = a;
+    }
+}
+
+void FormNewArray(string[] newArray, string[] oldArray)
+{
+    int count = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+
+        if (oldArray[i].Length <= 3)
+        {
+            newArray[count] = oldArray[i];
+            count++;
+        }
+    }
+    Array.Resize(ref newArray, newArray.Length);
+}
+
+Console.Clear();
+Console.WriteLine("Сколько планируем строк?");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] oldArray = new string[n];
+string[] newArray = new string[n];
+InPutArray(oldArray, n);
+FormNewArray (newArray, oldArray);
+Console.WriteLine($"Вуа-ля: [{string.Join(", ", newArray)}]");
+
+
+
